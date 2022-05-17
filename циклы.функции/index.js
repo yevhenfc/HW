55c5a0a249fb0f95cf68033200d0344644f2f5f5
yevhen <yevhenfc@gmail.com>
2022-05-17 19:22:55 +0300

    js1

--- циклы.функции/index.js
+++ циклы.функции/index.js
@@ -1,60 +1,3 @@
-// prompt('2+2') == 4 ? alert('+'):alert('-')
-
-// let i=1, sum=0,answ, res;
-// // for(i=1;i<=19;i=i+2){sum = sum + i};
- 
-// // do {answ=prompt('2+2')} while (answ!=4)
-
-// while (i<20){sum=sum+i;i=i+2}
-// alert(sum);
-
-// function add(a,b){return Number(a)+Number(b)};
-// function sub(a,b){return a-b};
-// function mult(a,b){return a*b};
-// function devi(a,b){return a/b};
-// function print(res){console.log(res)}
-
-// let a,b,c;
-// a = prompt('enter a');
-// b = prompt('enter b');
-// // do {
-// // c = prompt('choose act, 1-4');
-// // switch (c) {
-// //     case 1: 
-// //             print(add(a,b));
-// //             break;
-// //     case 2:
-// //             print(sub(a,b));
-// //             break;
-// //     case 3:
-// //             print(mult(a,b));
-// //             break;
-// //     case 4:
-// //             print(devi(a,b));
-// //             break;
-// //     default: console.log('try againe')
-// //     }
-// // } while (c!=5);
-// console.log(add(a,b));
-// console.log(sub(a,b));
-// console.log(mult(a,b));
-// console.log(devi(a,b));
-
-
-// function fn_1(num,mas){
-//     for (i = 1; i <= num; i++) {console.log(mas + ', ' + i)}
-// };
-// fn_1(5, 'hello');
-
-// function star(symb,num){
-//     while (0 < num) {
-//         console.log(symb.repeat(num));
-//         num--;
-//     }
-// };
-
-// star('$',10);
-
 // 1. ConsoleVar
 function consoleVar(){
     let vInt=11, vBool=true, vStr1='"java script"', vStr2='"100"';
@@ -63,57 +6,58 @@ function consoleVar(){
     console.log(vStr1);
     console.log(vStr2);
 };
-consoleVar();
+//-------------------------------------------------------------------------------------------------
+// 2. Greeting
+function greetName(userName = prompt('Ваше имя?')){
+    let greeting = 'Привет';
+    console.log(greeting + ', ' + userName + '!')
+}
+//-------------------------------------------------------------------------------------------------
+// 3. четверть часа
+function quarterHour(minutes = prompt('Введите минуты')){
+let res = (minutes/60) % 1;
+switch(true){
+    case res < 0.25: console.log('1-я четверть часа'); break; 
+    case res < 0.5:  console.log('2-я четверть часа'); break; 
+    case res < 0.75: console.log('3-я четверть часа'); break; 
+    default:         console.log('4-я четверть часа'); break;
+    }
+};
+//-------------------------------------------------------------------------------------------------
+// 4. кратность
+function multNumber(minLim   = prompt('Нижняя граница =')
+                   ,maxLim   = prompt('Верхняя граница =')
+                   ,multNumb = prompt('Кратность =')){
+    while (minLim <= maxLim) {
+        minLim % multNumb == 0 ? console.log(minLim) : 1;
+        minLim++; 
+    };
+}
+//-------------------------------------------------------------------------------------------------
+// 5. умножение
+function multSeries(minLim = prompt('Нижняя граница =')
+                   ,maxLim = prompt('Верхняя граница =')){
+let result = 1;
+for(minLim; minLim <= maxLim; minLim ++){result *= minLim}   
+console.log(result);
+};
+//-------------------------------------------------------------------------------------------------
+// проверка выбора
+function checkChoice(taskChoice){
+    Number.isInteger(taskChoice) == true && (taskChoice >= 1 && taskChoice <= 5) ? true : (alert('необходимо число (1-5)'), hwMenu())        
+}
+//-------------------------------------------------------------------------------------------------
+// меню
+function hwMenu(taskChoice = parseInt(prompt('Выберите номер задания (1-5)'),10)){
+checkChoice(taskChoice);
+switch (taskChoice) {
+    case 1:  consoleVar();  break;
+    case 2:  greetName();   break;
+    case 3:  quarterHour(); break;
+    case 4:  multNumber();  break;
+    default: multSeries();  break;
+    }
+}
+//-------------------------------------------------------------------------------------------------
 
-
-// // 2. Greeting
-// function greetName(userName = prompt('Ваше имя?')){
-//     let greeting = 'Привет';
-//     console.log(greeting + ', ' + userName + '!')
-// }
-// greetName();
-
-// // 3. четверть часа
-
-// function quarterHour(minutes = prompt('Минуты')){
-// let res = (minutes/60) % 1;
-// switch(true){
-//     case res < 0.25: 
-//         console.log('1-я четверть часа');
-//         break; 
-//     case (res >= 0.25 && res < 0.5):
-//         console.log('2-я четверть часа'); 
-//         break; 
-//     case (res >= 0.5 && res < 0.75):
-//         console.log('3-я четверть часа');
-//         break; 
-//     default: 
-//         console.log('4-я четверть часа');
-//         break;
-//     }
-// };
-// quarterHour();
-
-// // 4. кратность
-// function multNumber(minLim = prompt('Нижняя граница =')
-//                  ,maxLim = prompt('Верхняя граница =')
-//                  ,multNumb = prompt('Кратность =')){
-//     while (minLim <= maxLim) {
-//         minLim % multNumb == 0 ? console.log(minLim) : 1;
-//         minLim++; 
-//     };
-// }
-// multNumber();
-
-// // 5. умножение
-// function multSeries(minLim = prompt('Нижняя граница =')
-//              ,maxLim = prompt('Верхняя граница =')){
-// let result = 1;
-// for(minLim; minLim <= maxLim; minLim ++){result *= minLim}   
-// console.log(result);
-// };
-// multSeries();
-
-
-
-
+hwMenu();
