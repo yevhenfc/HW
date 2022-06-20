92088c0f6d08ae75159995d4a1eeee76b586d06b
yevhen <yevhenfc@gmail.com>
2022-06-20 07:09:58 +0300

    3

--- DOM base/index.js
+++ DOM base/index.js
@@ -19,7 +19,7 @@ document.body.append(hwUl);
 arrDays.forEach(el =>
    {let li = document.createElement('li');
    li.innerText = el;
-   hwUl.appendChild(li);
+   hwUl.append(li);
    }
 );
 //-------------------------------------------------------------------------------------------------
@@ -29,6 +29,49 @@ arr.forEach(el => el.style.backgroundColor = '#' + Math.floor(Math.random()*1677
 //-------------------------------------------------------------------------------------------------
 // 3* по просьбе трудящихся :)
 // объект Товара (название, цена, другие характеристики ...) создайте карточку
+let hwProdUl = document.createElement('ul');
+hwProdUl.classList.add('ProdUl');
+// hwProdUl.style.list-style-type: ";
+document.body.append(hwProdUl);
+
+function prodCard(objProd){
+   let card = document.createElement('li');
+   card.classList.add('Container');
+   
+   let cardDiv = document.createElement('div');
+   cardDiv.classList.add('cardDiv');
+   cardDiv.style.height = '150px';
+   cardDiv.style.width = '300px';
+   cardDiv.style.border = '1px solid black';
+
+   let cardContent = document.createElement('div');
+   cardDiv.classList.add('cardContent');
+   
+   let prName = document.createElement('p');
+   cardDiv.classList.add('prName');
+   prName.innerText = 'Name: ' + objProd.prName;
+
+   let prCategory = document.createElement('p');
+   cardDiv.classList.add('prCategory');
+   prCategory.innerText = 'Category: ' + objProd.prCategory;
+
+   let prAge = document.createElement('p');
+   cardDiv.classList.add('prAge');
+   prAge.innerText = 'Age: ' + objProd.prAge;
+
+   let prProducer = document.createElement('p');
+   cardDiv.classList.add('prProducer');
+   prProducer.innerText = 'Producer: ' + objProd.prProducer;
+
+   cardContent.append(prName);
+   cardContent.append(prCategory);
+   cardContent.append(prAge);
+   cardContent.append(prProducer);
+   cardDiv.append(cardContent);
+   card.append(cardDiv);
+   return card;
+}
+
 class Product{
    constructor(prName, prCategory, prAge, prProducer){
       this.prName     = prName;
@@ -46,3 +89,4 @@ let Prod4 = new Product('prName4','prCategory4','prAge4','prProducer4');
 let arrProd = [];
 arrProd.push(Prod1, Prod2, Prod3, Prod4);
 
+arrProd.forEach(el => hwProdUl.append(prodCard(el)));
