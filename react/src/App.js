ab070b4bc7f51444c585345ee513bcf10d52035e
yevhen <yevhenfc@gmail.com>
2022-07-24 22:33:51 +0300

    2

---
+++ react/src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import React_class_component from './Components/React_class_component';
+
+export default class App extends Component {
+render() {
+    return (
+      <React_class_component />
+     )
+  }
+}
+
---
+++ react/src/Components/React_class_component/index.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Task1 from './Task1';
+import Task2 from './Task2';
+
+export default class React_class_component extends Component {
+render() {
+    return (
+      <>
+        <Task1 />
+        <Task2 />
+      </>
+     )
+  }
+}
